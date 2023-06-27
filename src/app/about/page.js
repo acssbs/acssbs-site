a845a894ea0ffc4cb4e18c208eb9ea78f6c02747
João Paulo <joaopaulo1094@gmail.com>
2023-06-27 12:03:14 -0300

    Adicionado o Conteúdo da Página.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,14 @@
+import styles from '@/app/about/about.module.css'
+
 export default function About() {
     return (
-        <main>
-            <h1>Sobre a Associação</h1>
-        </main>
+        <div className={styles.About}>
+            <div className={styles.Container}>
+                <h1 className={styles.Title}>Sobre a Associação Comunitária de Surdos de São Bento do Sul.</h1>
+                <p className={styles.Text}>A Associação Comunitária de Surdos de São Bento do Sul (ACSSBS) foi fundada em 24/04/2022 na cidade de São Bento do Sul. Nossa associação tem como objetivo principal promover a inclusão e o bem-estar dos surdos em nossa comunidade. Através de ações e projetos, buscamos garantir o acesso à educação, cultura, lazer, saúde e outros direitos fundamentais para os surdos.</p>
+                <p className={styles.Text}>A ACSSBS é formada por um grupo de pessoas comprometidas com a causa dos surdos e estamos sempre em busca de parcerias e voluntários para ampliar nosso impacto e realizar ainda mais pela comunidade surda.</p>
+                <p className={styles.Text}>Seja você também parte desse movimento inclusivo e venha conhecer mais sobre a ACSSBS e nossas atividades. Junte-se a nós nessa jornada em prol da inclusão e da valorização dos surdos em São Bento do Sul.</p>
+            </div>
+        </div>
     )
 }
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,41 @@
-export default function About() {
+import Link from 'next/link'
+import Image from 'next/image'
+
+import styles from '@/app/contact/contact.module.css'
+
+export default function Contact() {
     return (
-        <main>
-            <h1>Contato</h1>
-        </main>
+        <div className={styles.Contact}>
+            <div className={styles.Container}>
+                <h1 className={styles.Title}>Contato:</h1>
+                <p className={styles.Text}>Agradecemos o seu interesse em entrar em contato conosco! A Associação Comunitária de Surdos de São Bento do Sul (ACSSBS) está disponível para receber suas dúvidas, sugestões, solicitações e qualquer outro tipo de comunicação.</p>
+                <p className={styles.Text}>Você pode entrar em contato conosco de diversas formas:</p>
+                <ul>
+                    <li className={styles.ListItem}>
+                        <p className={styles.TextItem}>
+                            <span className={styles.ContactIcon}>email</span> E-mail:
+                        </p>
+                        <p className={styles.TextItem}>acssbs.site@gmail.com</p>
+                    </li>
+                    <li className={styles.ListItem}>
+                        <p className={styles.TextItem}>
+                            <span className={styles.ContactIcon}>smartphone</span> Telefone:
+                        </p>
+                        <p className={styles.TextItem}>+55 (47) 9 9694-4755</p>
+                    </li>
+                    <li className={styles.ListItem}>
+                        <p className={styles.TextItem}>
+                            <Image className={styles.MediaIcon} src="/icons/instagram.svg" width={12} height={12} alt="Logo do Instagram" />
+                            Instagram:
+                        </p>
+                        <Link className={styles.LinkItem} href="https://www.instagram.com/acssbs/" target="_blank">
+                            <p className={styles.TextItem} style={{ marginBottom: 8 }}>@acssbs</p>
+                        </Link>
+                    </li>
+                </ul>
+                <p className={styles.Text}>Estamos sempre abertos para ouvir você e trabalhar juntos em prol da comunidade surda. Fique à vontade para nos contatar e teremos o prazer em ajudar.</p>
+                <p className={styles.Text}>Agradecemos pela sua colaboração e aguardamos o seu contato!</p>
+            </div>
+        </div>
     )
 }
